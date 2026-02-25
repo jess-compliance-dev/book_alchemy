eecdb5e55c6a1537fc868459209ca6d6729f323a
jess-compliance-dev <jessica.kaschade@gmx.de>
2026-02-25 14:58:58 +0100

    create library authors, backend Flask routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request
 from data_models import db, Author, Book
 
 
@@ -16,6 +16,37 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 # Flask mit SQLAlchemy verbinden
 db.init_app(app)
 
-
+# Tabellen erstellen
 with app.app_context():
-  db.create_all()
+    db.create_all()
+
+
+@app.route("/add_author", methods=["GET", "POST"])
+def add_author():
+    message = None
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        birth_date = request.form.get("birth_date")
+        date_of_death = request.form.get("date_of_death")
+
+        new_author = Author(
+            name=name,
+            birth_date=birth_date,
+            date_of_death=date_of_death
+        )
+
+        db.session.add(new_author)
+        db.session.commit()
+
+        message = "Author successfully added."
+
+    return render_template("add_author.html", message=message)
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# with app.app_context():
+#   db.create_all()
